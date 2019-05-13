80787f1f083d4d314b0a3ac5b5ce9d50430dfdd0
Inanna Craig-Morse <inanna.craigmorse@gmail.com>
2019-05-13 15:36:27 +0100

    restaurant review app

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,39 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+restaurants_attributes = [
+  {
+    name: 'Hoxton 100',
+    address: '7777 main st',
+    phone_number:'88 66 55 44',
+    category:'french'
+  },
+  {
+    name: 'Itsu',
+    address: 'kinslgand rd',
+    phone_number:'22 33 44 55',
+    category:'japanese'
+  },
+  {
+    name: 'La Nonna',
+    address: 'union st',
+    phone_number:'11 22 33 44',
+    category:'italian'
+  },
+  {
+    name:'India Club',
+    address: 'strand rd',
+    phone_number:'34 45 45 45',
+    category:'chinese'
+  },
+  {
+    name: 'Radio Rooftop',
+    address: 'strand rd',
+    phone_number:'23 34 45 54',
+    category:'belgian'
+  }
+]
+Restaurant.create!(restaurants_attributes)
+puts 'Finished!'
